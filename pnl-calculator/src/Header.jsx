97ebb46a07cbc26e86d06b336fbc7e73777e578c
connor <connorcliff211@gmail.com>
2024-10-10 09:08:53 +0100

    first commit

---
+++ pnl-calculator/src/Header.jsx
@@ -0,0 +1,21 @@
+function Header(){
+
+    return(
+        <>
+        <header>
+            <h1>Profit & Loss Calculator</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        </>
+    )
+
+}
+
+export default Header;
